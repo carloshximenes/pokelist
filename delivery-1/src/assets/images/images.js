0198ff109433c9c8da4ae7bd786a97fbed081e3a
Carlos Henrique <carloshximenes@gmail.com>
2023-05-04 22:53:51 -0300

    organize all images in one file

---
+++ delivery-1/src/assets/images/images.js
@@ -0,0 +1,61 @@
+import Arcanine from "./Arcanine.png";
+import Beedrill from "./Beedrill.png";
+import Blastoise from "./Blastoise.png";
+import Bulbasaur from "./Bulbasaur.png";
+import Butterfree from "./Butterfree.png";
+import Caterpie from "./Caterpie.png";
+import Charizard from "./Charizard.png";
+import Charmander from "./Charmander.png";
+import Charmeleon from "./Charmeleon.png";
+import Clefable from "./Clefable.png";
+import Dewgong from "./Dewgong.png";
+import Farfetchd from "./Farfetchd.png";
+import Ivysaur from "./Ivysaur.png";
+import Kadabra from "./Kadabra.png";
+import Kakuna from "./Kakuna.png";
+import Metapod from "./Metapod.png";
+import Pidgeot from "./Pidgeot.png";
+import Pidgeotto from "./Pidgeotto.png";
+import Pidgey from "./Pidgey.png";
+import Pikachu from "./Pikachu.png";
+import Ponyta from "./Ponyta.png";
+import Raichu from "./Raichu.png";
+import Raticate from "./Raticate.png";
+import Rattata from "./Rattata.png";
+import Squirtle from "./Squirtle.png";
+import Venusaur from "./Venusaur.png";
+import Vulpix from "./Vulpix.png";
+import Wartortle from "./Wartortle.png";
+import Weedle from "./Weedle.png";
+
+export default [
+  Arcanine,
+  Beedrill,
+  Blastoise,
+  Bulbasaur,
+  Butterfree,
+  Caterpie,
+  Charizard,
+  Charmander,
+  Charmeleon,
+  Clefable,
+  Dewgong,
+  Farfetchd,
+  Ivysaur,
+  Kadabra,
+  Kakuna,
+  Metapod,
+  Pidgeot,
+  Pidgeotto,
+  Pidgey,
+  Pikachu,
+  Ponyta,
+  Raichu,
+  Raticate,
+  Rattata,
+  Squirtle,
+  Venusaur,
+  Vulpix,
+  Wartortle,
+  Weedle,
+];
