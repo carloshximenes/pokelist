dbf56728fc13905628a43154f16c04871f9fdd91
Carlos Henrique <carloshximenes@gmail.com>
2023-05-04 22:55:02 -0300

    add news components into App

--- delivery-1/src/App.jsx
+++ delivery-1/src/App.jsx
@@ -1,8 +1,20 @@
-
-import './App.css'
+import "./App.css";
+import pokemonList from "./assets/images/images";
+import { getPokemonNameFromImage } from "./utils/pokemon.util";
+import Card from "./components/Card";
+import List from "./components/List";
 
 const App = () => {
-  return <></>
-}
+  return (
+    <>
+      <h1>Pokemon List</h1>
+      <List>
+        {pokemonList.map((src, index) => (
+          <Card key={index} image={src} name={getPokemonNameFromImage(src)} />
+        ))}
+      </List>
+    </>
+  );
+};
 
 export default App;
